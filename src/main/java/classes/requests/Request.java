3d0bc9d4f1a2d8f043a737773c3f1674698d0b58
Poddubniy Sergey <poddubniysergey1981@gmail.com>
2022-06-27 23:32:54 +0300

    Theme 'Forms and formats', task 3, feature/multipart

--- src/main/java/classes/requests/Request.java
+++ src/main/java/classes/requests/Request.java
@@ -1,8 +1,16 @@
 package classes.requests;
 
+import org.apache.commons.fileupload.*;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.portlet.PortletFileUpload;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.hc.core5.http.NameValuePair;
 import org.apache.hc.core5.net.WWWFormCodec;
 
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -11,14 +19,18 @@ import java.util.stream.Collectors;
 
 public class Request {
 
+    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
     private final String method;
     private final String path;
     private final List<NameValuePair> queryParams;
     private final List<NameValuePair> postParams;
     private final List<String> headers;
+    List<FileItem> parts;
     private final String body;
 
-    public Request(String[] requestLine, List<String> headers, String body) {
+    public Request(String[] requestLine, List<String> headers, String body) throws URISyntaxException, FileUploadException {
+        this.parts = new ArrayList<>();
         this.queryParams = new ArrayList<>();
         this.postParams = new ArrayList<>();
         this.method = requestLine[0];
@@ -31,14 +43,43 @@ public class Request {
         } else {
             this.path = requestLine[1];
         }
-        if (headers.stream()
-                .filter(header -> header.startsWith("Content-Type"))
-                .anyMatch(header -> header.contains("x-www-form-urlencoded"))
+        final var contentType = this.getContentTypeHeader();
+        if (contentType != null
+                && !contentType.isEmpty()
                 && this.body != null
                 && !this.body.isEmpty()
         ) {
-            this.postParams.addAll(WWWFormCodec.parse(this.body, StandardCharsets.UTF_8));
+            if (contentType.contains("x-www-form-urlencoded")) {
+                this.postParams.addAll(WWWFormCodec.parse(this.body, StandardCharsets.UTF_8));
+            }
+            if (contentType.contains("multipart/form-data")) {
+                this.parts.addAll(this.parseMultipart(contentType.substring(contentType.indexOf(" ") + 1)));
+            }
         }
+    }
+
+    private String getContentTypeHeader() {
+        return this.headers.stream()
+                .filter(header -> header.startsWith("Content-Type"))
+                .findFirst().orElse(null);
+    }
+
+    private List<FileItem> parseMultipart(String contentType) throws FileUploadException {
+        final var parameterParser = new ParameterParser();
+        parameterParser.setLowerCaseNames(true);
+        final var charset = parameterParser.parse(contentType, ';').get("charset");
+        final var requestContext = new RequestContextImpl(
+                charset != null && !charset.isEmpty() ? charset : DEFAULT_CHARSET.displayName(),
+                contentType,
+                this.body.getBytes());
+        if (ServletFileUpload.isMultipartContent(requestContext)) {
+            final var fileUploadBase = new PortletFileUpload();
+            final var fileItemFactory = new DiskFileItemFactory();
+            fileUploadBase.setFileItemFactory(fileItemFactory);
+            fileUploadBase.setHeaderEncoding(requestContext.getCharacterEncoding());
+            return fileUploadBase.parseRequest(requestContext);
+        }
+        return new ArrayList<>();
     }
 
     public String getBody() {
@@ -71,5 +112,13 @@ public class Request {
 
     public List<NameValuePair> getPostParam(String name) {
         return this.postParams.stream().filter(param -> param.getName().equals(name)).collect(Collectors.toList());
+    }
+
+    public List<FileItem> getParts() {
+        return this.parts;
+    }
+
+    public FileItem getPart(String name) {
+        return this.parts.stream().filter(part -> part.getFieldName().equals(name)).findFirst().orElse(null);
     }
 }
