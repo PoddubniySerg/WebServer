ee433111179b290a492e8fc23bdbb90d0b9b528a
Poddubniy Sergey <poddubniysergey1981@gmail.com>
2022-07-01 22:09:52 +0300

    fixed using the deprecated class URLEncoded

--- src/main/java/classes/requests/Request.java
+++ src/main/java/classes/requests/Request.java
@@ -29,6 +29,14 @@ public class Request {
         } else {
             this.path = requestLine[1];
         }
+        if (headers.stream()
+                .filter(header -> header.startsWith("Content-Type"))
+                .anyMatch(header -> header.contains("x-www-form-urlencoded"))
+                && this.body != null
+                && !this.body.isEmpty()
+        ) {
+            this.params.addAll(WWWFormCodec.parse(this.body, StandardCharsets.UTF_8));
+        }
     }
 
     public String getBody() {
